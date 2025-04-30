a08a2dafa8c5a33f0e845ea3469dd61f8fc9c327
abdelkaderbzz <abdelkaderbzz@users.noreply.github.com>
2025-04-30 14:56:48 +0100

    feat: Integrate Taki Popups plugin
    - Added `taki-popups-plugin-dev` to package.json.
    - Implemented Taki Popups in App.tsx with configuration for user metadata.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "next-themes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import ProductsPage from "./pages/ProductsPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
-import CartPage from "./pages/CartPage";
-import CheckoutPage from "./pages/CheckoutPage";
-import ProfilePage from "./pages/ProfilePage";
-import CategoriesPage from "./pages/CategoriesPage";
-import AboutUsPage from "./pages/AboutUsPage";
-import WishlistPage from "./pages/WishlistPage";
-import NotFound from "./pages/NotFound";
-import { CartProvider } from "./contexts/CartContext";
-import { WishlistProvider } from "./contexts/WishlistContext";
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { ThemeProvider } from 'next-themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import ProductsPage from './pages/ProductsPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import ProfilePage from './pages/ProfilePage';
+import CategoriesPage from './pages/CategoriesPage';
+import AboutUsPage from './pages/AboutUsPage';
+import WishlistPage from './pages/WishlistPage';
+import NotFound from './pages/NotFound';
+import { CartProvider } from './contexts/CartContext';
+import { WishlistProvider } from './contexts/WishlistContext';
+import { TakiPopups } from 'taki-popups-plugin-dev';
 
 const queryClient = new QueryClient();
 
+TakiPopups({
+  name: 'Adem',
+  memberId: '73924',
+  appId: '67ffaeade783d3021f53c288',
+  lang: 'ar',
+  meta_data: {
+    age: 18,
+    state: 'Manouba',
+    phoneNumber: '5289452343',
+    // ... additional meta-data
+  },
+});
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="light">
+      <ThemeProvider attribute='class' defaultTheme='light'>
         <CartProvider>
           <WishlistProvider>
             <TooltipProvider>
@@ -31,17 +43,17 @@ const App = () => {
               <Sonner />
               <BrowserRouter>
                 <Routes>
-                  <Route path="/" element={<Index />} />
-                  <Route path="/products" element={<ProductsPage />} />
-                  <Route path="/product/:id" element={<ProductDetailPage />} />
-                  <Route path="/cart" element={<CartPage />} />
-                  <Route path="/checkout" element={<CheckoutPage />} />
-                  <Route path="/profile" element={<ProfilePage />} />
-                  <Route path="/categories" element={<CategoriesPage />} />
-                  <Route path="/about" element={<AboutUsPage />} />
-                  <Route path="/wishlist" element={<WishlistPage />} />
+                  <Route path='/' element={<Index />} />
+                  <Route path='/products' element={<ProductsPage />} />
+                  <Route path='/product/:id' element={<ProductDetailPage />} />
+                  <Route path='/cart' element={<CartPage />} />
+                  <Route path='/checkout' element={<CheckoutPage />} />
+                  <Route path='/profile' element={<ProfilePage />} />
+                  <Route path='/categories' element={<CategoriesPage />} />
+                  <Route path='/about' element={<AboutUsPage />} />
+                  <Route path='/wishlist' element={<WishlistPage />} />
                   {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                  <Route path="*" element={<NotFound />} />
+                  <Route path='*' element={<NotFound />} />
                 </Routes>
               </BrowserRouter>
             </TooltipProvider>
